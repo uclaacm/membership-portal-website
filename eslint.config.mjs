d647c552a349fe49af2409f38ea3263d0a51d07c
Yeadam Kim <yes752044@gmail.com>
2024-11-29 18:24:01 -0800

    Update ESLint config
    - jsx-scope off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,14 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ];
